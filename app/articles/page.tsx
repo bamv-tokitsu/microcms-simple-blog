d703f017f217615baebf2caa830b7b30749a79e8
bamv-tokitsu <tokitsu@bamv.co.jp>
2025-07-01 17:34:28 +0900

    20250701-tokitsu01 記事一覧をarticles配下に移す

---
+++ app/articles/page.tsx
@@ -0,0 +1,31 @@
+import { Metadata } from 'next';
+import { getList } from '@/libs/microcms';
+import { LIMIT } from '@/constants';
+import Pagination from '@/components/Pagination';
+import ArticleList from '@/components/ArticleList';
+
+type Props = {
+  params: Promise<{
+    current: string;
+  }>;
+};
+
+export async function generateMetadata(props: Props): Promise<Metadata> {
+  const params = await props.params;
+  return {
+    alternates: {
+      canonical: `/articles/`,
+    },
+  };
+}
+export default async function Page() {
+  const data = await getList({
+    limit: LIMIT,
+  });
+  return (
+    <>
+      <ArticleList articles={data.contents} />
+      <Pagination totalCount={data.totalCount} basePath='/articles/'/>
+    </>
+  );
+}
