c585491a88cc11186f310b77a8e957a68c31daa9
bamv-tokitsu <tokitsu@bamv.co.jp>
2025-07-11 16:49:49 +0900

    埋め込みテスト2(フォーム修正版)

--- components/TopPage/index.tsx
+++ components/TopPage/index.tsx
@@ -2,6 +2,7 @@ import ArticleList from '@/components/ArticleList';
 import TagList from '@/components/TagList';
 import { Article, Tag } from '@/libs/microcms';
 import styles from './index.module.css';
+import { useRef } from "react";
 
 type Props = {
   data?: Article[];
@@ -10,18 +11,78 @@ type Props = {
 };
 
 export default function TopPage({ data, tags, tagData } : Props){
+  const submitted = useRef(false);
   return (
     <>
       <a href={`/articles/`}>記事一覧</a>
       <div className={styles.toparticle}>
         <ArticleList articles={data} />
       </div>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScnP7q8qWi1o2h9c9WBjP46J4W1c-7hmdYEG0_dbCS0Jecg_w/viewform?embedded=true" width={640} height={652} frameBorder={0} marginHeight={0} marginWidth={0}>読み込んでいます…</iframe>
       <TagList tags={tags}/>
       
       <div className={styles.toptagarticle}>
         <ArticleList articles={tagData} />
       </div>
+      <p style={{ textAlign: "center", margin: "40px auto" }}>
+        お気軽にお問い合わせください。
+      </p>
+      <form 
+      action="https://docs.google.com/forms/u/0/d/e/1FAIpQLScnP7q8qWi1o2h9c9WBjP46J4W1c-7hmdYEG0_dbCS0Jecg_w/formResponse"
+      method="POST"
+      target="hidden_iframe"
+      onSubmit={() => { submitted.current = true; }}
+        className="c-form">
+        <div className="c-form__item">
+          <label className="c-form__label" htmlFor="field-name">
+            名前・会社名<span className="c-form__required">必須</span>
+          </label>
+          <input
+            name="entry.1759742332"
+            className="c-form__input"
+            id="field-name"
+            placeholder="株式会社 山田"
+            type="text"
+            required
+          />
+        </div>
+        <div className="c-form__item">
+          <label className="c-form__label" htmlFor="field-mail">
+            メールアドレス<span className="c-form__required">必須</span>
+          </label>
+          <input
+            name="entry.611978144"
+            className="c-form__input"
+            id="field-mail"
+            placeholder="sample@gmail.com"
+            type="email"
+            required
+          />
+        </div>
+        <div className="c-form__item">
+          <label className="c-form__label" htmlFor="field-message">
+            メッセージ内容
+          </label>
+          <textarea
+            name="entry.611978144"
+            className="c-form__input"
+            id="field-message"
+            placeholder="お問い合わせ内容"
+          />
+        </div>
+        <div className="c-form__submit">
+          <button type="submit">送信する</button>
+        </div>
+      </form>
+      <iframe
+        name="hidden_iframe"
+        id="hidden_iframe"
+        style={{ display: "none" }}
+        onLoad={() => {
+          if (submitted.current) {
+            window.location.href = "/articles"; // thanksページのURL
+          }
+        }}
+      />
     </>
   )
 };
